9d2b357d2937a9ca28ba71228448db62cafbe9a1
viniciusgneris <viniciusneris@usp.br>
2021-10-25 15:46:37 -0300

    Initial commit

---
+++ outroprojeto/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == 'light-theme') {
+        this.textContent = "Noturno";
+    }
+    else{
+        this.textContent = "Claro";
+    }
+
+});
---
+++ php/email.php
@@ -0,0 +1,19 @@
+<?php 
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+
+$duvemp = addslashes($_POST['groups'])
+$duvger = addslashes($_POST['groups'])
+$scadm = addslashes($_POST['groups'])
+$surec = addslashes($_POST['groups'])
+$don = addslashes($_POST['groups'])
+$nome = addslashes($_POST['name'])
+$email = addslashes($_POST['email'])
+$mensagem = addslashes($_POST['mesage'])
+
+
+
+}
+
+?>
